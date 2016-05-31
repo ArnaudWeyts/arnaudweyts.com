0b98877887601256658551e2f0d3181d7f348dda
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-05-31 20:44:48 +0200

    complete website revamp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
 	jade = require('gulp-jade'),
 	sass = require('gulp-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
-	cssnano = require('cssnano'),
+	cssnano = require('gulp-cssnano'),
+	imagemin = require('gulp-imagemin'),
 	browserSync = require('browser-sync').create();
 
 gulp.task('jade', function() {
@@ -18,14 +19,15 @@ gulp.task('jade', function() {
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.scss')
+  return gulp.src('./src/sass/**/styles.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
     	browsers: ['>1%'],
     	cascade: false
     }))
     .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('browser-sync', () => {
@@ -33,12 +35,23 @@ gulp.task('browser-sync', () => {
 	  	server: {
 	  		baseDir: './',
 	  		index: 'index.html'
-	  	}
+	  	},
+	  	notify: false
   	});
+});
+
+gulp.task('copy', function () {
+	gulp.src('./node_modules/font-awesome/fonts/*')
+		.pipe(gulp.dest('./assets/fonts'));
+	gulp.src('./src/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./assets/img'));
 });
 
 gulp.task('watch', function () {
 	gulp.watch('./src/jade/*.jade', ['jade']).on('change', browserSync.reload);
 	gulp.watch('./src/sass/**/*.scss', ['sass']).on('change', browserSync.reload);
 });
+
+gulp.task('default', ['watch', 'jade', 'copy', 'sass', 'browser-sync']);
 
