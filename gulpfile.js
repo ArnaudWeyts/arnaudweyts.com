5eeddd3220eb17164a8a0ea3acdc7dc6f75df3ee
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-02 12:01:05 +0200

    added skills with kinda working js(it's shitty doe)

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
-	jade = require('gulp-jade'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	cssnano = require('gulp-cssnano'),
-	imagemin = require('gulp-imagemin'),
-	browserSync = require('browser-sync').create();
+jade = require('gulp-jade'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+cssnano = require('gulp-cssnano'),
+imagemin = require('gulp-imagemin'),
+concat = require('gulp-concat'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
 
 gulp.task('jade', function() {
 	var YOUR_LOCALS = {};
@@ -19,39 +21,50 @@ gulp.task('jade', function() {
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/styles.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-    	browsers: ['>1%'],
-    	cascade: false
-    }))
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css'))
-    .pipe(browserSync.stream());
+	return gulp.src('./src/sass/**/styles.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['>1%'],
+		cascade: false
+	}))
+	.pipe(cssnano())
+	.pipe(gulp.dest('./assets/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('scripts', function() {
+	return gulp.src(['./node_modules/material-design-lite/material.js','./src/js/*.js'])
+	.pipe(concat('main.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('./assets/js/'))
+});
+
+gulp.task('images', function() {
+	return gulp.src('./src/img/**/*', {base: './src/img'})
+	.pipe(imagemin())
+	.pipe(gulp.dest('./assets/img/'))
 });
 
 gulp.task('browser-sync', () => {
-  browserSync.init({
-	  	server: {
-	  		baseDir: './',
-	  		index: 'index.html'
-	  	},
-	  	notify: false
-  	});
+	browserSync.init({
+		server: {
+			baseDir: './',
+			index: 'index.html'
+		},
+		notify: false
+	});
 });
 
 gulp.task('copy', function () {
 	gulp.src('./node_modules/font-awesome/fonts/*')
-		.pipe(gulp.dest('./assets/fonts'));
-	gulp.src('./src/img/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./assets/img'));
+	.pipe(gulp.dest('./assets/fonts'))
 });
 
 gulp.task('watch', function () {
-	gulp.watch('./src/jade/*.jade', ['jade']).on('change', browserSync.reload);
-	gulp.watch('./src/sass/**/*.scss', ['sass']).on('change', browserSync.reload);
+	gulp.watch('./src/jade/**/*.jade', ['jade']).on('change', browserSync.reload)
+	gulp.watch('./src/sass/**/*.scss', ['sass']).on('change', browserSync.reload)
+	gulp.watch('./src/js/**/*.js', ['scripts']).on('change', browserSync.reload)
 });
 
-gulp.task('default', ['watch', 'jade', 'copy', 'sass', 'browser-sync']);
+gulp.task('default', ['watch', 'jade', 'copy', 'sass', 'scripts', 'browser-sync']);
 
