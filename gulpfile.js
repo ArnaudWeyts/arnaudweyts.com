cc1087aa53109e95a6a9377427a81b5b51d5207a
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-06 23:26:07 +0200

    some codacy fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,86 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp'),
-browserify = require('browserify'),
-source = require('vinyl-source-stream'),
-buffer = require('vinyl-buffer'),
-sourcemaps = require('gulp-sourcemaps'),
-gutil = require('gulp-util'),
-jade = require('gulp-jade'),
-sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-cssnano = require('gulp-cssnano'),
-imagemin = require('gulp-imagemin'),
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify'),
-browserSync = require('browser-sync').create();
+var gulp = require("gulp"),
+browserify = require("browserify"),
+source = require("vinyl-source-stream"),
+buffer = require("vinyl-buffer"),
+sourcemaps = require("gulp-sourcemaps"),
+gutil = require("gulp-util"),
+jade = require("gulp-jade"),
+sass = require("gulp-sass"),
+autoprefixer = require("gulp-autoprefixer"),
+cssnano = require("gulp-cssnano"),
+imagemin = require("gulp-imagemin"),
+concat = require("gulp-concat"),
+uglify = require("gulp-uglify"),
+browserSync = require("browser-sync").create();
 
-gulp.task('jade', function() {
+gulp.task("jade", function() {
     var YOUR_LOCALS = {};
 
-    gulp.src('./src/jade/*.jade')
+    gulp.src("./src/jade/*.jade")
     .pipe(jade({
         locals: YOUR_LOCALS
     }))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest("./"))
 });
 
-gulp.task('sass', function () {
-    return gulp.src('./src/sass/**/styles.scss')
-    .pipe(sass().on('error', sass.logError))
+gulp.task("sass", function () {
+    return gulp.src("./src/sass/**/styles.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer({
-        browsers: ['>1%'],
+        browsers: [">1%"],
         cascade: false
     }))
     .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css'))
+    .pipe(gulp.dest("./assets/css"))
     .pipe(browserSync.stream())
 });
 
-gulp.task('scripts', function() {
+gulp.task("scripts", function() {
     // set up the browserify instance on a task basis
     var b = browserify({
-        entries: './src/js/scripts.js',
+        entries: "./src/js/scripts.js",
         debug: true
     });
 
     return b.bundle()
-    .pipe(source('main.js'))
+    .pipe(source("main.js"))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
         // Add transformation tasks to the pipeline here.
         .pipe(uglify())
-        .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./assets/js/'));
+        .on("error", gutil.log)
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("./assets/js/"));
 });
 
-gulp.task('images', function() {
-    return gulp.src('./src/img/**/*', {base: './src/img'})
+gulp.task("images", function() {
+    return gulp.src("./src/img/**/*", {base: "./src/img"})
     .pipe(imagemin())
-    .pipe(gulp.dest('./assets/img/'))
+    .pipe(gulp.dest("./assets/img/"))
 });
 
-gulp.task('browser-sync', () => {
+gulp.task("browser-sync", () => {
     browserSync.init({
         server: {
-            baseDir: './',
-            index: 'index.html'
+            baseDir: "./",
+            index: "index.html"
         },
         notify: false
     });
 });
 
-gulp.task('copy', function () {
-    gulp.src('./node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('./assets/fonts'))
+gulp.task("copy", function () {
+    gulp.src("./node_modules/font-awesome/fonts/*")
+    .pipe(gulp.dest("./assets/fonts"))
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./src/jade/**/*.jade', ['jade']).on('change', browserSync.reload)
-    gulp.watch('./src/sass/**/*.scss', ['sass']).on('change', browserSync.reload)
-    gulp.watch('./src/js/**/*.js', ['scripts']).on('change', browserSync.reload)
+gulp.task("watch", function () {
+    gulp.watch("./src/jade/**/*.jade", ["jade"]).on("change", browserSync.reload);
+    gulp.watch("./src/sass/**/*.scss", ["sass"]).on("change", browserSync.reload);
+    gulp.watch("./src/js/**/*.js", ["scripts"]).on("change", browserSync.reload);
 });
 
-gulp.task('default', ['watch', 'jade', 'copy', 'sass', 'scripts', 'browser-sync']);
+gulp.task("default", ["watch", "jade", "copy", "sass", "scripts", "browser-sync"]);
 
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,23 @@
-require('material-design-lite');
+require("material-design-lite");
 window.onload = function() {
-  "use strict"
+  "use strict";
 
   // animations on skills
   var elements = document.getElementsByClassName("stroke");
   var intervals = [];
+
   function clear(i) {
     return function(){
       clearInterval(intervals[i]);
+    };
+  }
+  function clearAll() {
+    for (var i = intervals.length - 1; i >= 0; i--) {
+      clearInterval(intervals[i]);
     }
+  }
+  function stroke(elem) {
+    elem.setAttribute("stroke-dashoffset", parseInt(elem.getAttribute("stroke-dashoffset"))-3);
   }
   for (var i = elements.length - 1; i >= 0; i--) {
     var timer = Math.round(15 / parseInt(elements[i].getAttribute("data-amount")) * 252);
@@ -20,16 +29,7 @@ window.onload = function() {
         stroke(elem);
       }
     }, timer, elements[i], i);
-  };
-  var stroke = function(elem) {
-    elem.setAttribute("stroke-dashoffset", parseInt(elem.getAttribute("stroke-dashoffset"))-3);
   }
-  function clearAll() {
-    for (var i = intervals.length - 1; i >= 0; i--) {
-      clearInterval(intervals[i]);
-    }
-  }
-
   // clear all intervals anyways, because apparently they keep running on forever
   // btw this is really shitty
   setTimeout(clearAll, 2500);
